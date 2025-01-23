285a5733fd18915d893c3d5c4b1b50173f7df33b
KSH7-7 <ssafy12ksh@gmail.com>
2025-01-23 10:39:56 +0900

    [refactor]api사실상 삭제 및 정리

---
+++ src/app/admin/api/climate/route.js
@@ -0,0 +1,15 @@
+// API기능이었던 흔적
+
+// // import { NextResponse } from 'next/server';
+
+// export async function GET() {
+//   try {
+//     // const response = await fetch('http://70.12.246.128:8000/climate');
+//     // if (!response.ok) throw new Error('날씨현황 불러오기 실패');
+//     // const data = await response.json();
+//     // return NextResponse.json(data);
+//   } 
+//   catch (error) {
+//     // return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
