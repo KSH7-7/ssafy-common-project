409c43b5b7cf1f9f416629eb052faac79be8778f
KSH7-7 <ssafy12ksh@gmail.com>
2025-02-10 16:08:46 +0900

    [refactor]코드 개선

--- src/app/admin/control/[robot_id]/JoystickWrapper.tsx
+++ src/app/admin/control/[robot_id]/JoystickWrapper.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+// JoystickWrapper.tsx
 
 const JoystickModule = require("react-joystick-component");
-// default export가 있으면 사용, 없으면 전체 모듈을 사용합니다.
-const Joystick = JoystickModule.default || JoystickModule;
+
+// `JoystickModule`이 객체이므로, 실제로 `Joystick`을 꺼내야 함
+const Joystick = JoystickModule.Joystick ?? JoystickModule.default ?? JoystickModule;
 
 export default Joystick;
