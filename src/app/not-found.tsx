0fe12e8d122bac5691c3504661eeeabe02422ba0
KSH7-7 <ssafy12ksh@gmail.com>
2025-02-10 15:12:41 +0900

    [feat]리스트 연동 & 로그인 구현

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function NotFound() {
   return (
@@ -7,21 +6,18 @@ export default function NotFound() {
       <div className="container mx-auto px-4">
         <div className="text-center">
           <div className="relative h-[400px] bg-center bg-no-repeat bg-cover mb-[-50px]"
-               style={{backgroundImage: "url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif)"}}>
+               style={{ backgroundImage: "url(/404.jpg)" }}>
             <h1 className="absolute inset-0 flex items-center justify-center text-8xl font-bold">
               404
             </h1>
           </div>
-          
+
           <div className="mt-12">
-            <h3 className="text-2xl font-bold mb-4">
-              Look like you're lost
-            </h3>
-            
+
             <p className="mb-8">
               The page you are looking for is not available!
             </p>
-            
+
             <Link href="/" 
                   className="inline-block px-5 py-2 bg-[#39ac31] text-white hover:bg-[#2d8a26] transition-colors">
               Go to Home
@@ -32,4 +28,3 @@ export default function NotFound() {
     </div>
   )
 }
-
