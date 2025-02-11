0e4a9da8ebe5ec2e545a952c81f41cb35862fb43
Wona7286 <wona7286@naver.com>
2025-02-11 20:14:43 +0900

    modify

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 echo "Deploying the application..."
 
-cd /home/ubuntu/S12P11A207 || exit
+cd /home/ubuntu/S12P11A207/207/BackEnd/Docker || exit
 
+echo "🛑 Stopping existing containers..."
 docker-compose down
 
+echo "🧹 Removing unused Docker images..."
 docker image prune -f
 
+echo "📥 Pulling latest code..."
 git pull origin master
 
+echo "🚀 Rebuilding and restarting containers..."
 docker-compose up -d --build
 
-echo "Deployment complete"
+echo "✅ Deployment complete!"
