321f81268fdb0256f85a645a5796f4163238c774
kim taewon <kktw102@naver.com>
2025-02-17 10:10:11 +0900

    merge오류 수정

--- src/app/admin/control/[robot_id]/JoystickWrapper.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-// JoystickWrapper.tsx
-
-const JoystickModule = require("react-joystick-component");
-
-// `JoystickModule`이 객체이므로, 실제로 `Joystick`을 꺼내야 함
-const Joystick = JoystickModule.Joystick ?? JoystickModule.default ?? JoystickModule;
-
-export default Joystick;
