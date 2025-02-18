58cc4d469a410d573006b17f45acc174dc404397
kim taewon <kktw102@naver.com>
2025-02-18 17:36:43 +0900

    탭 icon, 이름 변경

--- 207/FrontEnd/src/app/layout.tsx
+++ 207/FrontEnd/src/app/layout.tsx
@@ -6,8 +6,11 @@ import PathCheck from "./PathCheck";
 import { LanguageProvider } from "./contexts/LanguageContext"
 // use client를 사용한다면 서버 컴포넌트 관련된 에러가 뜸 -> PathCheck를 임포트하여 use client 관련 기능은 분리
 export const metadata: Metadata = {
-  title: "SMART LockeR",
+  title: "KEEPRO",
   description: "Smart locker management system",
+  icons: {
+    icon: '/favicon.png', // public 폴더 경로 기준
+  },
 };
 
 export default function RootLayout({
